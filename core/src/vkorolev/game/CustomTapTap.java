9bb3e6e8eff0593ae501674fe070eb9811f8e026
Вадим Королев <vkorolev@diasoft.ru>
2022-07-25 10:05:21 +0300

    Перенос функционала базовой игры для последующего изменения. (ни один проект же не делается с нуля :) )

--- core/src/vkorolev/game/CustomTapTap.java
+++ core/src/vkorolev/game/CustomTapTap.java
@@ -1,31 +1,29 @@
 package vkorolev.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class CustomTapTap extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class CustomTapTap extends Game {
+    SpriteBatch batch;
+    BitmapFont font;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        batch.dispose();
+        font.dispose();
+    }
 }
